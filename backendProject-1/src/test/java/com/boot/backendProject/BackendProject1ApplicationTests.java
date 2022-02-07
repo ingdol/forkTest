a88ad9a6910f24f3aa1f90bb11e5cafdea8eda34
신은지 <dmswl3073@naver.com>
2022-02-07 13:50:25 +0900

    commit

---
+++ backendProject-1/src/test/java/com/boot/backendProject/BackendProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.backendProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
